8e54849b5b61d87a12747d4721734c9190aacc57
LiaJa <63929806+LiaJa@users.noreply.github.com>
2020-05-31 16:41:24 +0200

    Update guess_homework.py

--- guess_homework.py
+++ guess_homework.py
@@ -8,22 +8,22 @@ current_time = datetime.datetime.now()
 secret = random.randint(1, 30)
 attempts = 0
 
-with open("scorehw_list.txt", "r") as scorehw_file:
-    scorehw_list = json.loads(scorehw_file.read())
-    for scorehw_dict in scorehw_list:
-        print(str(scorehw_dict["attempts"]) + "  attempts for " +  scorehw_dict.get("player_name") + " on date: " + scorehw_dict.get("datetime") + " with secret number: " + str(scorehw_dict.get("secret_number")) + " and " + str(scorehw_dict.get("wrong_guesses")) + " wrong guesses ")
+with open("scorehow_list.txt", "r") as scorehow_file:
+    scorehow_list = json.loads(scorehow_file.read())
+    for scorehow_dict in scorehow_list:
+        print(str(scorehow_dict["attempts"]) + "  attempts for " +  scorehow_dict.get("player_name") + " on date: " + scorehow_dict.get("datetime") + " with secret number: " + str(scorehow_dict.get("secret_number")) + " and "  + str(scorehow_dict.get("wrong_guesses")) + " wrong guessess ")
 
-wrong_guesses = []
+wrong_guesses=[]
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
-        scorehw_data = {"attempts": attempts, "datetime": str(current_time), "player_name": player, "secret_number": secret, "wrong_guesses": wrong_guesses}
-        scorehw_list.append(scorehw_data)
+        scorehow_data = {"attempts": attempts, "datetime": str(current_time), "player_name": player, "secret_number": secret,"wrong_guesses": wrong_guesses}
+        scorehow_list.append(scorehow_data)
 
-        with open("scorehw_list.txt", "w") as scorehw_file:
-            scorehw_file.write(json.dumps(scorehw_list))
+        with open("scorehow_list.txt", "w") as scorehow_file:
+            scorehow_file.write(json.dumps(scorehow_list))
 
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
@@ -33,3 +33,4 @@ while True:
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
 
+    wrong_guesses.append(guess)
